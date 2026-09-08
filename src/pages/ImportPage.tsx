3c2372b9d8ae4e5008c46641625fe05bdc56ee1e
Report-Engainall <ahadnanmo@gmail.com>
2026-09-08 16:07:15 +0300

    feat(import): expose governed mapping dictionary

--- src/pages/ImportPage.tsx
+++ src/pages/ImportPage.tsx
@@ -2,12 +2,14 @@ import { ExternalFileAnalysisPage } from '@/pages/ExternalFileAnalysisPage';
 import { CanonicalImportPage } from '@/pages/CanonicalImportPage';
 import { FolderBatchImportPanel } from '@/components/FolderBatchImportPanel';
 import { ImportIntelligenceCockpit } from '@/components/ImportIntelligenceCockpit';
+import { ImportMappingDictionaryPanel } from '@/components/ImportMappingDictionaryPanel';
 
 export function ImportPage() {
   return <div className="space-y-6">
     <ImportIntelligenceCockpit />
     <ExternalFileAnalysisPage />
     <CanonicalImportPage />
+    <ImportMappingDictionaryPanel />
     <FolderBatchImportPanel />
   </div>;
 }
