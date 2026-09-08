b1d231bb5b893e11462365b8eb4ba14a1eab53e5
Report-Engainall <ahadnanmo@gmail.com>
2026-09-08 15:48:38 +0300

    feat(import): expose universal external analysis above canonical import

--- src/pages/ImportPage.tsx
+++ src/pages/ImportPage.tsx
@@ -1,8 +1,10 @@
+import { ExternalFileAnalysisPage } from '@/pages/ExternalFileAnalysisPage';
 import { CanonicalImportPage } from '@/pages/CanonicalImportPage';
 import { FolderBatchImportPanel } from '@/components/FolderBatchImportPanel';
 
 export function ImportPage() {
   return <div className="space-y-6">
+    <ExternalFileAnalysisPage />
     <CanonicalImportPage />
     <FolderBatchImportPanel />
   </div>;
