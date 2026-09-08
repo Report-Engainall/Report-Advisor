efbebb97d12c7edfca3917fd3fbac8a3268c9ba3
Report-Engainall <ahadnanmo@gmail.com>
2026-09-08 16:05:27 +0300

    feat(import): surface data intelligence cockpit

--- src/pages/ImportPage.tsx
+++ src/pages/ImportPage.tsx
@@ -1,9 +1,11 @@
 import { ExternalFileAnalysisPage } from '@/pages/ExternalFileAnalysisPage';
 import { CanonicalImportPage } from '@/pages/CanonicalImportPage';
 import { FolderBatchImportPanel } from '@/components/FolderBatchImportPanel';
+import { ImportIntelligenceCockpit } from '@/components/ImportIntelligenceCockpit';
 
 export function ImportPage() {
   return <div className="space-y-6">
+    <ImportIntelligenceCockpit />
     <ExternalFileAnalysisPage />
     <CanonicalImportPage />
     <FolderBatchImportPanel />
