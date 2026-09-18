03da27dd14b5705406788c271393deb4579b79dc
Report-Engainall <ahadnanmo@gmail.com>
2026-09-18 05:20:46 +0300

    test(pdf): polyfill Uint8Array toHex for pdfjs Node runtime

--- scripts/check-pdf-structured-regression.ts
+++ scripts/check-pdf-structured-regression.ts
@@ -3,6 +3,15 @@ import { createServer, type ViteDevServer } from 'vite';
 if (!('DOMMatrix' in globalThis)) Object.defineProperty(globalThis, 'DOMMatrix', { configurable: true, value: class DOMMatrix {} });
 type PromiseConstructorWithTry = PromiseConstructor & { try?: (fn: (...args: unknown[]) => unknown, ...args: unknown[]) => Promise<unknown> };
 const promiseConstructor = Promise as PromiseConstructorWithTry;
+if (!('toHex' in Uint8Array.prototype)) {
+  Object.defineProperty(Uint8Array.prototype, 'toHex', {
+    configurable: true,
+    value: function toHex(this: Uint8Array): string {
+      return Array.from(this, (byte) => byte.toString(16).padStart(2, '0')).join('');
+    },
+  });
+}
+
 if (typeof promiseConstructor.try !== 'function') {
   Object.defineProperty(Promise, 'try', {
     configurable: true,
